c5fc53dcb59a8438aef8961d3e2617b071c20981
eddiemetrium <eddiemitch262@gmail.com>
2025-02-14 09:38:27 -0500

    footer height added

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -6,10 +6,10 @@ import React from "react";
 export default function Footer() {
   return (
     <footer className="bg-slate-700 py-6 mt-4 pt-5 max-w-full">
-      <div className="container flex justify-center items-center mx-auto px-4 text-center text-white"></div>{" "}
+      <div className="container flex justify-center items-center mx-auto px-4 text-center text-white h-16">
       <p className="text-sm text-slate-200 mx-auto flex justify-center items-center ">
         © {new Date().getFullYear()} Splash Academy. All rights reserved.
-      </p>
+      </p></div>{" "}
     </footer>
   );
 }
